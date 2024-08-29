c430a74247f65630192fa7736c0d965d5f28df80
Giedrius <giedrius.mail.11@gmail.com>
2024-08-29 17:33:41 +0200

    Testing module change

--- src/uproot/models/RNTuple.py
+++ src/uproot/models/RNTuple.py
@@ -518,7 +518,7 @@ in file {self.file.file_path}"""
             [self.read_col_page(ncol, i) for i in cluster_range], axis=0
         )
         print(f"[uproot] DEBUG: len(res) after numpy.concatenate: {len(res)}")
-        print(f"[uproot] DEBUG: res: {numpy.tolist(res)}")
+        print(f"[uproot] DEBUG: res: {res.tolist()}")
         if pad_missing_ele:
             first_ele_index = self.column_records[ncol].first_ele_index
             res = numpy.pad(res, (first_ele_index, 0))
