497a9d68355556a14621e4571aa81acc14036088
Giedrius <giedrius.mail.11@gmail.com>
2024-08-29 17:31:43 +0200

    Testing module change

--- src/uproot/models/RNTuple.py
+++ src/uproot/models/RNTuple.py
@@ -518,8 +518,7 @@ in file {self.file.file_path}"""
             [self.read_col_page(ncol, i) for i in cluster_range], axis=0
         )
         print(f"[uproot] DEBUG: len(res) after numpy.concatenate: {len(res)}")
-        numpy.set_printoptions(threshold=20000)  # Increase threshold to display more elements
-        print(f"[uproot] DEBUG: res: {res}")
+        print(f"[uproot] DEBUG: res: {numpy.to_list(res)}")
         if pad_missing_ele:
             first_ele_index = self.column_records[ncol].first_ele_index
             res = numpy.pad(res, (first_ele_index, 0))
