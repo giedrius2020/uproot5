6a25015a96402114337c1e9036e2b3de97a112d4
Giedrius <giedrius.mail.11@gmail.com>
2024-08-30 14:33:46 +0200

    Fixing offsets

--- src/uproot/models/RNTuple.py
+++ src/uproot/models/RNTuple.py
@@ -517,8 +517,8 @@ in file {self.file.file_path}"""
 
         if is_offset_col:
             # Calculate cumulative lengths and add offsets
-            offset_endings = numpy.concatenate(([0], [arr[:-1].flatten() for arr in arrays[:-1]]))
-            print("DEBUG offsets: ", offset_endings)
+            offsets = [0] + [len(arr) for arr in arrays[:-1]]
+            print("DEBUG offsets: ", offsets)
 
             # Add the offsets to each array
             adjusted_arrays = [arr + offset for arr, offset in zip(arrays, offsets)]
